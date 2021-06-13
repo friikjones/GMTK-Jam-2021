a6afe0821817454f253b16215b13735fffa9a964
amebiki <amebikii@gmail.com>
2021-06-13 18:29:02 +0200

    postprocess and scene

---
+++ Assets/Scripts/Rotating_Script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating_Script : MonoBehaviour
+{
+
+    public Vector3 rotating_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        this.transform.Rotate(rotating_speed *Time.deltaTime, Space.Self);
+        
+    }
+}
